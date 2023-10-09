ec76f8e212b9b1d8959f0b00aabe315115df6521
Odds <panerjeffryjames@gmail.com>
2023-10-10 01:06:23 +0800

    added welcome screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,33 +3,49 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 
 
 function fetchCatImage() {
+    $('.cat-img-container').hide();
+    $('.loading').show();
+
+    $('.cat-img').attr('src', '../img/placeholder.png');
     fetch(`${BASE_URL}/images/search`, {
         headers: {
             'x-api-key': API_KEY
         }
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
-            // console.log(data[0]);
             const url = data[0].url;
 
             localStorage.setItem('catImageUrl', url);
-
             $('.cat-img').attr('src', url);
+
+            $('.loading').hide();
+            $('.cat-img-container').show();
         })
         .catch(error => {
             console.error('Error fetching cat data:', error);
+            $('.loading').hide();
         });
 }
 
 const storedUrl = localStorage.getItem('catImageUrl');
-
 if (storedUrl) {
     $('.cat-img').attr('src', storedUrl);
 } else {
     fetchCatImage;
 }
 
+setTimeout(() => {
+    $('.welcome-container').hide();
+    setTimeout(() => {
+        fetchCatImage()
+    }, 100)
+}, 2000)
 $('#fetch-cat').on("click", () => {
     localStorage.clear();
     fetchCatImage();
